abeccd2a8a412aec5a879006cacf2eda93be9ea6
SUMIT SAURAV <52030057+sumitsaurav143@users.noreply.github.com>
2021-07-13 00:23:40 +0530

    Update guessmcq.js

--- guessmcq.js
+++ guessmcq.js
@@ -1,12 +1,17 @@
 var ans;
 var choosen;
 var sum=0;
-
+var t=10;
+var tt;
 function start()
 {
+    t=11;
     document.getElementById("popup").style.display="none";
     document.getElementById("restarter").style.display="block";
+    document.getElementById("tm").style.display="block";
     ans=Math.floor((Math.random() * 10) + 1);
+    tt=setInterval(timer,1000);
+
     //document.getElementById("qh").innerHTML="Guess Correct Number";
     //document.getElementById("ques").style.background="white";
     document.getElementById("op1").innerHTML=ans;
@@ -19,10 +24,26 @@ function start()
     document.getElementById("op3").style.order=Math.floor((Math.random() * 10) + 1);
     document.getElementById("op4").style.order=Math.floor((Math.random() * 10) + 1);
     
+}
+
+function timer()
+{
+    t-=1;
+    document.getElementById("timer").innerText=t;
+    if(t<1){
+        clearTimeout(tt);
+        var wrong=new Audio("Wrong.mp3");
+        wrong.play();
+        document.getElementById("popup").style.background="Red";
+        document.getElementById("popup").style.display="";
+        document.getElementById("res").innerHTML="Times Up!!";
+    }
     
 }
+
 function s1(){
     choosen=document.getElementById("op1").innerHTML;
+    clearTimeout(tt);
     if(ans==choosen)
     {
         var correct=new Audio("correct.mp3");
@@ -46,6 +67,7 @@ function s1(){
 }
 
 function s2(){
+    clearTimeout(tt);
     choosen=document.getElementById("op2").innerHTML;
     if(ans==choosen)
     {
@@ -70,6 +92,7 @@ function s2(){
 }
 
 function s3(){
+    clearTimeout(tt);
     choosen=document.getElementById("op3").innerHTML;
     if(ans==choosen)
     {
@@ -94,6 +117,7 @@ function s3(){
 }
 
 function s4(){
+    clearTimeout(tt);
     choosen=document.getElementById("op4").innerHTML;
     if(ans==choosen)
     {
